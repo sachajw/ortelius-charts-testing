65d3c9aca982536bd59ff048e456dae3a164bee7
Sacha Wharton <extern_wharton_sacha@allianz.de>
2023-03-06 10:41:34 +0200

    👌 IMPROVE: readme testing

--- readme-merge.py
+++ readme-merge.py
@@ -1,8 +1,32 @@
 import os
 
-readme_files = [f for f in os.listdir() if f.startswith('README') and f.endswith('.md')]
+# Define the root directory where README files are located
+root_directory = "./"
 
-with open('README.md', 'w') as outfile:
-    for fname in readme_files:
-        with open(fname) as infile:
-            outfile.write(infile.read())
+# Define a function that recursively finds all README files in subdirectories
+def find_readme_files(directory):
+    readme_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == "README.md":
+                readme_files.append(os.path.join(root, file))
+    return readme_files
+
+# Call the function to find all README files
+readme_files = find_readme_files(root_directory)
+
+# Define a function to merge multiple README files into one file
+def merge_readme_files(readme_files):
+    merged_content = ""
+    for file in readme_files:
+        with open(file, "r") as f:
+            content = f.read()
+            merged_content += content + "\n\n"
+    return merged_content
+
+# Call the function to merge all README files
+merged_content = merge_readme_files(readme_files)
+
+# Write the merged content to a new file called "MERGED_README.md" in the root directory
+with open(os.path.join(root_directory, "MERGED_README.md"), "w") as f:
+    f.write(merged_content)
